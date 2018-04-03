8c47b19c1c2a6158807b3e0a93a63f6362ba4dac
Jomar Santos <jomaroliversantos@gmail.com>
2018-04-02 21:14:04 -0700

    some theming for main screen and host session

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import ReactNative from 'react-native'
 const {
+	StyleSheet,
+	StatusBar,
 	View,
 	Text,
 	Image,
@@ -10,37 +12,96 @@ import { connect } from 'react-redux'
 import { navigate } from '../actions/NavigationActions'
 import { logout } from '../actions/AuthActions'
 
+
 class HomeScreen extends Component {
 	render() {
 		return (
-			<View>
-				<View>
+			<View style={styles.main}>
+				<StatusBar hidden={true} />
+
+				<TouchableHighlight style={styles.logoutButton}
+					onPress={() => {this.props.logout()}}>
+					<Text style={styles.logoutText}>LOGOUT</Text>
+				</TouchableHighlight>
+				
+				<View style={styles.logo}>
 					<Image
+						resizeMode='center'
 	          source={require('../../assets/images/logo.png')}
 	        />
 				</View>
 
-				<View>
-						<TouchableHighlight
-							onPress={() => {this.props.navigate('JoinSession')}}
-						>
-							<Text>JOIN</Text>
+				<View style={styles.buttons}>
+						<TouchableHighlight style={styles.joinButton}
+							onPress={() => {this.props.navigate('JoinSession')}}>
+							<Text style={styles.joinText}>JOIN</Text>
 						</TouchableHighlight>
-						<TouchableHighlight
-							onPress={() => {this.props.navigate('HostSession')}}
-						>
-							<Text>HOST</Text>
-						</TouchableHighlight>
-						<TouchableHighlight
-							onPress={() => {this.props.logout()}}
-						>
-							<Text>LOGOUT</Text>
+						<TouchableHighlight style={styles.hostButton}
+							onPress={() => {this.props.navigate('HostSession')}}>
+							<Text style={styles.hostText}>HOST</Text>
 						</TouchableHighlight>
 				</View>
 			</View>
 		)
 	}
 }
+
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+		backgroundColor: '#000000',
+  },
+	logo: {
+		marginBottom: 30,
+  },
+	buttons: {
+	},
+	joinButton: {
+    paddingTop: 10,
+    paddingBottom: 10,
+		paddingLeft: 40,
+		paddingRight: 40,
+		marginBottom: 10,
+    backgroundColor:'#EDB106',
+		borderRadius: 20,
+		justifyContent: 'center',
+  },
+	hostButton: {
+		paddingTop: 10,
+    paddingBottom: 10,
+		paddingLeft: 40,
+		paddingRight: 40,
+		marginBottom: 10,
+		backgroundColor:'#000000',
+		borderRadius: 20,
+		borderWidth: 1,
+		borderColor: '#EDB106',
+		justifyContent: 'center',
+	},
+	logoutButton: {
+		position: 'absolute',
+		paddingTop: 20,
+		paddingBottom: 20,
+		paddingLeft: 20,
+		paddingRight: 20,
+		top: 0,
+		right: 0,
+	},
+	joinText: {
+		textAlign: 'center',
+		color: '#000000'
+	},
+	hostText: {
+		textAlign: 'center',
+		color: '#EDB106'
+	},
+	logoutText: {
+		textAlign: 'center',
+		color: '#EDB106'
+	}
+});
 
 HomeScreen.navigationOptions = {
   title: 'Home',
--- client/src/screens/HostSessionScreen.js
+++ client/src/screens/HostSessionScreen.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import ReactNative from 'react-native'
 const {
+	StyleSheet,
+	StatusBar,
 	View,
 	Text,
 	TouchableHighlight
@@ -27,9 +29,9 @@ class HostSessionScreen extends Component {
 		let participants = this.props.participants.map((participant, index) => {
 			if (!participant.hasOwnProperty('status')) {
 				return (
-					<View key={participant.id}>
+					<View key={participant.id} style={styles.participantCell}>
 						<View>
-							<Text>{participant.name}</Text>
+							<Text style={styles.participantText}>{participant.name}</Text>
 						</View>
 						<TouchableHighlight
 							onPress={() => {this.props.declineParticipant(participant.id)}}
@@ -42,27 +44,99 @@ class HostSessionScreen extends Component {
 		});
 
 		return (
-			<View>
-				<View>
-					<Text>{this.props.test}</Text>
-					<Text>Waiting for participants...</Text>
-					<View>
+			<View style={styles.main}>
+				<StatusBar hidden={true} />
+				<View style={styles.participants}>
+					<Text style={styles.text}>Waiting for participants...</Text>
+					<View style={styles.participantsList}>
 						{participants}
 					</View>
 				</View>
-				<View>
-					<Text>Shareable shooot code:</Text>
-					<Text>{this.props.session.code}</Text>
+				
+				<View style={styles.code}>
+					<Text style={styles.text}>Shareable shooot code:</Text>
+					<Text style={styles.codeText}>{this.props.session.code}</Text>
 				</View>
-				<TouchableHighlight
-					onPress={() => {this.props.navigate('RearrangeParticipants')}}
-				>
-					<Text>Next</Text>
-				</TouchableHighlight>
+				
+				<View style={styles.bottomBar}>
+					<TouchableHighlight
+						style={styles.nextButton}
+						onPress={() => {this.props.navigate('RearrangeParticipants')}}>
+						<Text style={styles.nextText}>NEXT</Text>
+					</TouchableHighlight>
+				</View>
 			</View>
 		)
 	}
 }
+
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+		backgroundColor: '#000000',
+  },
+	text: {
+		color: '#EDB106'
+	},
+	participants: {
+		flex: 4,
+		paddingTop: 40,
+		paddingLeft: 20,
+		paddingRight: 20,
+		alignSelf: 'stretch',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	participantsList: {
+		paddingTop: 10,
+		flex: 1,
+		alignSelf: 'stretch',
+	},
+	participantCell: {
+		paddingTop: 10,
+		paddingBottom: 10,
+		paddingLeft: 10,
+		paddingRight: 10,
+		marginBottom: 10,
+		borderRadius: 20,
+		alignSelf: 'stretch',
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor:'#EDB106',
+	},
+	participantText: {
+		color: '#000000'
+	},
+	code: {
+		alignItems: 'center',
+		justifyContent: 'center',
+		flex: 1
+	},
+	codeText: {
+		fontSize: 30,
+		color: '#EDB106'
+	},
+	bottomBar: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	nextText: {
+		color: '#000000'
+	},
+	nextButton: {
+		paddingTop: 10,
+    paddingBottom: 10,
+		paddingLeft: 40,
+		paddingRight: 40,
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor:'#EDB106',
+		borderRadius: 20,
+	}
+});
 
 HostSessionScreen.navigationOptions = {
   title: 'Host Shooot',
