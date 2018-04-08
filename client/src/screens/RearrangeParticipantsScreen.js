c8f0c3b39710042f7815aeaeb53ec7307cf0294b
Jomar Santos <jomaroliversantos@gmail.com>
2018-04-07 18:35:37 -0700

    rearrange participants theming

--- client/src/screens/RearrangeParticipantsScreen.js
+++ client/src/screens/RearrangeParticipantsScreen.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import ReactNative from 'react-native'
 const {
+	StyleSheet,
+	StatusBar,
 	View,
 	Text,
 	TouchableHighlight
@@ -13,26 +15,102 @@ class HostSessionScreen extends Component {
 		let participants = this.props.participants.map((participant, index) => {
 			if (!participant.hasOwnProperty('status')) {
 				return (
-					<View key={participant.id}>
-							<Text>{participant.name}</Text>
+					<View key={participant.id} style={styles.participantCell}>
+							<Text style={styles.participantText}>{participant.name}</Text>
 					</View>
 				);
 			}
 		});
 
 		return (
-			<View>
-				<Text>Rearrange Participants</Text>
-				{participants}
-				<TouchableHighlight
-					onPress={() => this.props.startSession(this.props.session, this.props.participants, this.props.socket)}
-				>
-					<Text>Next</Text>
-				</TouchableHighlight>
+			<View style={styles.main}>
+				<StatusBar hidden={true} />
+				<View style={styles.participants}>
+					<Text style={styles.text}>Rearrange Participants</Text>
+					<View style={styles.participantsList}>
+						{participants}
+					</View>
+				</View>
+				
+				<View style={styles.bottomBar}>
+					<TouchableHighlight
+						style={styles.nextButton}
+						onPress={() => this.props.startSession(this.props.session, this.props.participants, this.props.socket)}>
+						<Text style={styles.nextText}>START</Text>
+					</TouchableHighlight>
+				</View>
 			</View>
 		)
 	}
 }
+
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+		backgroundColor: '#000000',
+  },
+	text: {
+		color: '#EDB106'
+	},
+	participants: {
+		flex: 4,
+		paddingTop: 40,
+		paddingLeft: 20,
+		paddingRight: 20,
+		alignSelf: 'stretch',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	participantsList: {
+		paddingTop: 10,
+		flex: 1,
+		alignSelf: 'stretch',
+	},
+	participantCell: {
+		paddingTop: 10,
+		paddingBottom: 10,
+		paddingLeft: 10,
+		paddingRight: 10,
+		marginBottom: 10,
+		borderRadius: 20,
+		alignSelf: 'stretch',
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor:'#EDB106',
+	},
+	participantText: {
+		color: '#000000'
+	},
+	code: {
+		alignItems: 'center',
+		justifyContent: 'center',
+		flex: 1
+	},
+	codeText: {
+		fontSize: 30,
+		color: '#EDB106'
+	},
+	bottomBar: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	nextText: {
+		color: '#000000'
+	},
+	nextButton: {
+		paddingTop: 10,
+    paddingBottom: 10,
+		paddingLeft: 40,
+		paddingRight: 40,
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor:'#EDB106',
+		borderRadius: 20,
+	}
+});
 
 HostSessionScreen.navigationOptions = {
   title: 'Rearrange Participants',
